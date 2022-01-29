886505f0ce046b1c75582b6428dfb8cd683d2d2f
Cleber <cleberb@Clebers-iMac.lan>
2022-01-29 22:53:06 +0000

    backend commit

---
+++ src/main/java/net/javacleber/springboot/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package net.javacleber.springboot.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import net.javacleber.springboot.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
+
+}
